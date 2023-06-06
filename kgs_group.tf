309354aa482dea0ce8323fcdb312eba7cbb2a67a
your_git_username <gbadamosisaheedo@gmail.com>
2023-06-05 20:38:42 -0500

    roles and responsiblity for payabale for KGS Group

---
+++ kgs_group.tf
@@ -0,0 +1,50 @@
+KGSCareersAccounts Payable Clerk
+Accounts Payable Clerk
+LOCATION:
+Winnipeg
+DEPARTMENT:
+Administration
+DATE POSTED:
+May 31, 2023
+CLOSING DATE:
+Jun 14, 2023
+KGS Group is seeking an Accounts Payable Clerk to join our Accounting Department in Winnipeg.
+
+About Us
+We are KGS Group, an employee-owned, multidisciplinary engineering consulting firm serving clients throughout North America. We are different than traditional engineering firms. We believe a commitment to lasting relationships is just as critical as the expertise applied in our work. To us that means we put our clients and people first. We have the discipline to consistently deliver KGS Quality, never taking the easy way out. We always look closer and dig deeper. And no matter the project, we care enough to do it right.
+
+As an employee-owned company, we are invested in our work and our people. Working with us means you will be part of some of the most interesting projects across Canada, the U.S. and beyond. With the extensive, wide-ranging expertise within our company, you will have countless opportunities to learn and grow.
+
+The Role
+In this role you will assist in the day-to-day accounting functions for all offices across Canada. Under the supervision of the Assistant Controller, you will be responsible for entering and managing payments, reviewing discrepancies, and answering internal and external payment inquiries. You will be joining an accounting team of about 10 people in a flexible and professional environment and will have the opportunity to learn and grow within the role. 
+
+Key Responsibilities include but are not limited to:
+
+Accomplish full-cycle A/P
+Receive, review, verify and enter invoices for payment
+Generate aged accounts payable (AP) report weekly and inform supervisor of any significant issues
+Research and resolve invoice discrepancies and issues working with vendors and project managers
+Manage the process of verifying, logging, and mailing checks, including expediting special handling
+Review employee expense claims and process payments
+Accurately execute daily processes and controls in a timely manner while ensuring company policies are followed
+Assist in other accounting roles and duties as required
+Facilitate internal and external audits as needed
+Required to maintain client and company financial information confidential
+Develop effective relationships with stake holders such as Clients, Department Heads, Project Managers, and the accounting team in the resolution of roadblocks to payment
+About You
+You have a professional manner, excellent organizational, interpersonal and communication skills. You are also detail oriented with the flexibility to meet deadlines and changing priorities. You are technologically savvy with the ability to learn new systems and adapt to change quickly.
+
+Your Qualifications and Credentials:
+
+Minimum 2+ years of experience in accounts payable
+Completion of high school; post-secondary education would be considered an asset
+Customer service and ERP software experience is considered an asset
+Proficiency in Microsoft Office including Excel, Word, and Outlook
+Strong communication, problem solving, and analytical skills are required
+Ability to handle multiple tasks to prioritize needs and expedite tasks upon request
+Detail oriented and results driven in alignment with the KGS Culture
+KGS Group is an employee-owned equal opportunity company offering an excellent compensation and benefits package.
+
+KGS Group welcomes and encourages applications from people with disabilities. Upon request, KGS Group will provide reasonable accommodation for disabilities to support participation of candidates in all aspects of the recruitment process.
+
+ 
